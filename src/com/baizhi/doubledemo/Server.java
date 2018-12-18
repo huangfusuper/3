19f34f654adbe6b73e0d049b136dce3ac52d6974
huangfu <huangfusuper@163.com>
2018-12-18 18:06:00 +0800

    NIO10.0

---
+++ src/com/baizhi/doubledemo/Server.java
@@ -0,0 +1,13 @@
+package com.baizhi.doubledemo;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Server {
+    public static void main(String[] args) {
+        server();
+    }
+
+    public static void server(){
+        System.out.println("昝鹏霞我爱你");
+    }
+}
